de2188cc4d4373f64e56bf46c7801fb0d1a255f8
ejkim <ej.kim@gmail.com>
2020-05-26 15:37:15 +0900

    five

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,18 +30,18 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.menuBar = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuBar = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.studentreg1 = new UIMake.studentreg();
-            this.studentsearch1 = new UIMake.studentsearch();
             this.studentdel1 = new UIMake.studentdel();
+            this.studentsearch1 = new UIMake.studentsearch();
+            this.studentreg1 = new UIMake.studentreg();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,24 +57,16 @@
             this.panel1.Size = new System.Drawing.Size(181, 716);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // pictureBox3
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(29, 143);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(127, 58);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // menuBar
-            // 
-            this.menuBar.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.menuBar.Location = new System.Drawing.Point(12, 143);
-            this.menuBar.Name = "menuBar";
-            this.menuBar.Size = new System.Drawing.Size(15, 57);
-            this.menuBar.TabIndex = 1;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(30, 271);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(127, 58);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox2
             // 
@@ -87,16 +79,24 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // pictureBox3
+            // menuBar
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(30, 271);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(127, 58);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.menuBar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.menuBar.Location = new System.Drawing.Point(12, 143);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(15, 57);
+            this.menuBar.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(29, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // panel2
             // 
@@ -109,12 +109,12 @@
             this.panel2.Size = new System.Drawing.Size(658, 716);
             this.panel2.TabIndex = 1;
             // 
-            // studentreg1
+            // studentdel1
             // 
-            this.studentreg1.Location = new System.Drawing.Point(0, 0);
-            this.studentreg1.Name = "studentreg1";
-            this.studentreg1.Size = new System.Drawing.Size(658, 716);
-            this.studentreg1.TabIndex = 0;
+            this.studentdel1.Location = new System.Drawing.Point(0, 0);
+            this.studentdel1.Name = "studentdel1";
+            this.studentdel1.Size = new System.Drawing.Size(658, 716);
+            this.studentdel1.TabIndex = 2;
             // 
             // studentsearch1
             // 
@@ -123,12 +123,12 @@
             this.studentsearch1.Size = new System.Drawing.Size(658, 716);
             this.studentsearch1.TabIndex = 1;
             // 
-            // studentdel1
+            // studentreg1
             // 
-            this.studentdel1.Location = new System.Drawing.Point(0, 0);
-            this.studentdel1.Name = "studentdel1";
-            this.studentdel1.Size = new System.Drawing.Size(658, 716);
-            this.studentdel1.TabIndex = 2;
+            this.studentreg1.Location = new System.Drawing.Point(0, 0);
+            this.studentreg1.Name = "studentreg1";
+            this.studentreg1.Size = new System.Drawing.Size(658, 716);
+            this.studentreg1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -140,9 +140,9 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
